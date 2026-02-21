387a94161ba9386d1a98d01ab0a1880647a9704d
Jason3x <jason_2x@hotmail.com>
2026-02-21 06:49:11 +0100

    Add files via upload

---
+++ Realtek/installRealtek.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#-----------------------------------#
+#    Install Realtek Bluetooth      #
+#  + Wi-Fi firmware for dArkOS      #
+#            By Jason               #
+#-----------------------------------#
+
+if [ "$(id -u)" -ne 0 ]; then
+    exec sudo -E "$0" "$@"
+fi
+
+clear > /dev/tty1
+echo "--------------------------------------" > /dev/tty1
+echo "|   Realtek Firmware Installer       |" > /dev/tty1
+echo "|           By Jason                 |" > /dev/tty1
+echo "--------------------------------------" > /dev/tty1
+echo "" > /dev/tty1
+sleep 2
+
+sudo dpkg -i /roms/tools/Realtek/firmware-realtek_20250410-2_all.deb 2>&1 || sudo dpkg -i /roms2/tools/Realtek/firmware-realtek_20250410-2_all.deb 2>&1
+
+# Reload modules
+echo "Reloading Realtek kernel modules..." > /dev/tty1
+sleep 2
+
+MODULES=$(lsmod | awk '{print $1}' | grep -E '^rtl|^bt|^btrtl|^btusb|^btbcm|^btintel|^rtk|^rtw')
+
+for mod in $(echo "$MODULES" | tac); do
+    modprobe -r "$mod" 2>/dev/null || true
+done
+
+sleep 2
+
+for mod in $MODULES; do
+    modprobe "$mod" 2>/dev/null || true
+done
+
+echo "Kernel modules reloaded." > /dev/tty1
+sleep 2
+echo "" > /dev/tty1
+
+echo "Firmware installation completed." > /dev/tty1
+echo "System will power off now..." > /dev/tty1
+sleep 2
+
+shutdown now
